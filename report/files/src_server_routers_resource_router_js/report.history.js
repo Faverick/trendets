2f4531312bd6828b5f7c8260cf420d592bb83bfd
Travis-CI <a.a.pakhomoff@gmail.com>
2015-04-08 15:33:13 +0000

    Documentation updated

--- report/files/src_server_routers_resource_router_js/report.history.js
+++ report/files/src_server_routers_resource_router_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 08 Apr 2015 15:28:37 GMT","sloc":60,"lloc":35,"functions":10,"deliveredBugs":0.5154029819467043,"maintainability":82.2950488983246,"lintErrors":4,"difficulty":17.29032258064516}]
+__history = [{"date":"Wed, 08 Apr 2015 15:33:13 GMT","sloc":60,"lloc":35,"functions":10,"deliveredBugs":0.5154029819467043,"maintainability":82.2950488983246,"lintErrors":4,"difficulty":17.29032258064516}]
--- report/files/test_test_js/report.history.js
+++ report/files/test_test_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 08 Apr 2015 15:28:37 GMT","sloc":68,"lloc":36,"functions":13,"deliveredBugs":0.629606818608658,"maintainability":81.28195184099495,"lintErrors":14,"difficulty":15.333333333333334}]
+__history = [{"date":"Wed, 08 Apr 2015 15:33:13 GMT","sloc":68,"lloc":36,"functions":13,"deliveredBugs":0.629606818608658,"maintainability":81.28195184099495,"lintErrors":14,"difficulty":15.333333333333334}]

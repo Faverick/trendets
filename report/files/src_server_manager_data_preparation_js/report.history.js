9d394ff158661c855d6a02cd9e498d14c242e2c9
sansanch <a.a.pakhomoff@gmail.com>
2015-04-08 20:53:49 +0300

    Из develop удалена документация, по пушу теперь должна обновляться вся документация и не будет провоцировать новую сборку: #TD-21 tag теперь все доки в gh-pages и доступны на сайте

--- report/files/src_server_manager_data_preparation_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 08 Apr 2015 15:36:26 GMT","sloc":85,"lloc":40,"functions":14,"deliveredBugs":0.4842746332447992,"maintainability":85.00535099543592,"lintErrors":7,"difficulty":8.590909090909092}]
--- report/files/test_test_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 08 Apr 2015 15:36:26 GMT","sloc":68,"lloc":36,"functions":13,"deliveredBugs":0.629606818608658,"maintainability":81.28195184099495,"lintErrors":14,"difficulty":15.333333333333334}]

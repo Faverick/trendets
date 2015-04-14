b6282b7256e7a8123c3a0fa8259b6a27f9c9855b
Travis-CI <a.a.pakhomoff@gmail.com>
2015-04-14 07:14:33 +0000

    Documentation and reports updated

--- report/files/src_server_settings_js/report.history.js
+++ report/files/src_server_settings_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 14 Apr 2015 07:10:49 GMT","sloc":6,"lloc":4,"functions":0,"deliveredBugs":0.04905374780501027,"maintainability":73.92022786758693,"lintErrors":1,"difficulty":4.2}]
+__history = [{"date":"Tue, 14 Apr 2015 07:14:33 GMT","sloc":6,"lloc":4,"functions":0,"deliveredBugs":0.04905374780501027,"maintainability":73.92022786758693,"lintErrors":1,"difficulty":4.2}]

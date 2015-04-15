5e2b2068011872cfc2513a877bf6c7d1939ea9f1
Travis-CI <a.a.pakhomoff@gmail.com>
2015-04-15 17:22:00 +0000

    Documentation and reports updated

--- report/files/src_server_manager_start_default_js/report.history.js
+++ report/files/src_server_manager_start_default_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 14 Apr 2015 07:14:32 GMT","sloc":47,"lloc":27,"functions":4,"deliveredBugs":0.24625343909104194,"maintainability":92.30572547024734,"lintErrors":6,"difficulty":8.526315789473683}]
+__history = [{"date":"Wed, 15 Apr 2015 17:21:59 GMT","sloc":47,"lloc":27,"functions":4,"deliveredBugs":0.24625343909104194,"maintainability":92.30572547024734,"lintErrors":6,"difficulty":8.526315789473683}]

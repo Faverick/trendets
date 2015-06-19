9d5f271dfa5e7fb37d32d3b26ca907a86fad3f35
vesko.pavlov@yahoo.com <vesko.pavlov@yahoo.com>
2015-06-19 12:52:14 +0300

    Implemented answer as an array of stock objects to client request

--- src/server/routers/resource-router.js
+++ src/server/routers/resource-router.js
@@ -2,7 +2,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     rek = require('rekuire'),
     path = require('path'),
-    request = require('request');
+    request = require('request'),
+    papa = require('babyparse'),
+    async = require('async');
 
 var settings = rek('settings'),
     TrendetsDb = rek('db'),
@@ -62,36 +64,58 @@ function addRestStocksResource()
 
     router.route('/' + settings.resource)
         .post(function (req, res) {
-            var options = {
-                url: req.body['url'],
-                headers: {
-                    'Host': '195.128.78.52',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-                    'Accept-Encoding': 'null',
-                    'Referer': 'http://www.finam.ru/profile/forex/eur-usd/export/',
-                    'Connection': 'keep-alive'
+            async.parallel([
+                function (callback) {
+                    var allStocks = [];
+                    var options = {
+                        url: req.body['url'],
+                        headers: {
+                            'Host': '195.128.78.52',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
+                            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+                            'Accept-Encoding': 'null',
+                            'Referer': 'http://www.finam.ru/profile/forex/eur-usd/export/',
+                            'Connection': 'keep-alive'
+                        }
+                    }
+                    logger.info("about to send request for stocks");
+                    request.get(options, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            papa.parse(body, {
+                                worker: true,
+                                step: function(results) {
+                                    var stock = {
+                                        code: results.data[0][0],
+                                        period: results.data[0][1],
+                                        date: results.data[0][2],
+                                        time: results.data[0][3],
+                                        open: results.data[0][4],
+                                        high: results.data[0][5],
+                                        low: results.data[0][6],
+                                        close: results.data[0][7],
+                                        volume: results.data[0][8] 
+                                    }
+                                    allStocks.push(stock);
+                                }
+                            });
+                            callback(false, allStocks);
+                        } else{
+                            logger.error(error);
+                            console.log(error);
+                            callback(true); 
+                            return;
+                        }  
+                    });
+                },],
+                function(err, results) {
+                    if(err) { console.log(err); res.send(500,"Server Error"); return; }
+                    res.send(results[0]);
                 }
-            };
-            logger.info("about to send request for stocks");
-            request.get(options, callback);
+            );
         });
 
-    function callback(error, response, body) {
-        logger.info("entered callback");
-        if (!error && response.statusCode == 200) {
-            var allEvents = [];
-            // var json = JSON.parse(body);
-            // console.log(123123);
-            // console.log(json);
-            console.log(body);
-        } else{
-            logger.error(error);
-            console.log(error);
-        }
-        
-    }
+    
 }
 function responseJson(resp) {
     return function (result) {
